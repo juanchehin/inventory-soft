97da30a6e6c01f437212be1d04a305f6b86db524
chehin <chehinjuan@gmail.com>
2022-01-16 20:38:06 -0300

    modificacion de nombre y autorioa

--- config/author.php
+++ config/author.php
@@ -7,16 +7,13 @@ return [
     | Company/Author/Developer/Licence information of the application
     |--------------------------------------------------------------------------
     |
-    | All details about the author/developer/contact/licence of the application
-    |
-    | IMPORTANT: CHANGING ANY OF THIS INFORMATION WILL UNSTABILIZE THE APPLICATION
     |
     */
 
-    'vendor' => 'Ultimate Fosters',
-    'vendor_url' => 'http://ultimatefosters.com',
-    'email' => 'thewebfosters@gmail.com',
+    'vendor' => 'Slider Tucuman',
+    'vendor_url' => 'https://juanchehin.github.io/slider/',
+    'email' => 'slidetucuman@gmail.com',
     'app_version' => "2.14.3",
-    'lic1' => 'aHR0cHM6Ly9sLnVsdGltYXRlZm9zdGVycy5jb20vYXBpL3R5cGVfMQ==',
+    'lic1' => '',
     'pid' => 1,
 ];
